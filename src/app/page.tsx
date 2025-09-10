d5b5225acf9b7ce01942e85b93f886810a66ae6f
VictorZP <61043613+VictorZP@users.noreply.github.com>
2025-09-10 03:25:13 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,8 +60,9 @@ export default function Home() {
   };
 
   useEffect(() => {
-    fetchData();
+    fetchData({ season: '2024-2025' });
   }, []);
+
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900">
